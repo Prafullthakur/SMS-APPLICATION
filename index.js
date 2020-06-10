7cc85cc70d8e4ba384d80ab2329095d9b260e4fa
prafull <prafullchuahn45@gmail.com>
2020-06-10 11:00:55 -0400

    add

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ app.use(express.static(__dirname + '/public'));
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => {
+
+    console.log('Server started on port ${port}')
+});
