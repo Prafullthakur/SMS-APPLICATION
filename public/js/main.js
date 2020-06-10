c4c813b8634e9175abdb629321a53ad867f56809
prafull <prafullchuahn45@gmail.com>
2020-06-10 12:51:47 -0400

    add

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+const numberInput = document.getElementById('number');
+const textInput = document.getElementById('msg');
+const button = document.getElementById('button');
+const response = document.querySelector('.response');
